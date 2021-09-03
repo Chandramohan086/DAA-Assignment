a22b03b5f6bffd3ec75012cc0ee6f64d793c256b
Chandramohan086 <chandramohans086@gmail.com>
2021-09-03 21:38:23 +0530

    searching

---
+++ week1/linear_search.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,i,k,t;
+	cout<<"enter the test cases=";
+	cin>>t;
+	cout<<endl;
+	while(t--)
+	{
+		cout<<"Enter length of array=";
+		cin>>n;
+		int a[n];
+		cout<<"enter the elements of array=";
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		cout<<"Enter the key element=";
+		cin>>k;
+		for(i=0;i<n;i++)
+		{
+			if(k==a[i])
+			{
+			  cout<<"YES"<<endl<<"No. of comparisons="<<i+1;
+			  break;
+		    }
+		}
+		cout<<endl;
+		if(i==n)
+		{
+			cout<<"NO"<<endl<<"No. of comaprisons="<<n;
+			
+		}
+    }
+	return 0;
+}
